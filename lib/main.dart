58edca207948f8738292051cb1ff0c564cbf3b61
Kimberly Mae Montenegro <mkimberlymae0812@gmail.com>
2020-09-11 03:36:05 +0800

    first run

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,54 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(DrawerWidget());
-}
+void main() => runApp(MyApp());
 
-class DrawerWidget extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _DrawerWidgetState createState() => _DrawerWidgetState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
 }
 
-class _DrawerWidgetState extends State<DrawerWidget> {
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Drawer Widget"),
       ),
+      body: Center(child: Text('My Page!')),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
-          children: const <Widget>[
+          children: <Widget>[
             DrawerHeader(
+              child: Text('Drawer Header'),
               decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text(
-                'Drawer Header',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
+                  gradient: LinearGradient(
+                      colors: <Color>[Colors.orange[400], Colors.yellow[300]])),
             ),
             ListTile(
-              leading: Icon(Icons.message),
-              title: Text('Messages'),
+              leading: Icon(Icons.home),
+              title: Text("Home"),
+              onTap: () {
+                Navigator.pop(context);
+              },
             ),
             ListTile(
-              leading: Icon(Icons.account_circle),
-              title: Text('Profile'),
+              leading: Icon(Icons.account_box),
+              title: Text("Profile"),
+              onTap: () {
+                Navigator.pop(context);
+              },
             ),
             ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
+              leading: Icon(Icons.assignment),
+              title: Text("Campaign"),
+              onTap: () {
+                Navigator.pop(context);
+              },
             ),
           ],
         ),
